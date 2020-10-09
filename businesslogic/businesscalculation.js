923334709d3b7691a360f1f62a0fb3c061a45420
Your Name <heavenlyalienzonearth@github.com>
2020-10-09 21:33:56 +0530

    This is my first commit

---
+++ businesslogic/businesscalculation.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2) {
+    return (num1 + num2)
+}
+
+function multNumbers(num1, num2) {
+    return (num1 * num2)
+}
+
+function divNumbers(num1, num2) {
+    return (num1 / num2)
+}
+
+function minusNumbers(num1, num2) {
+    return (num1 - num2)
+}
